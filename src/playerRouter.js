08f5af341adc1195d208dcf8ddec104b5ecac43a
Jorge <sanchez.jorge02@gmail.com>
2023-12-09 22:08:11 +0100

    Práctica de Fundamentos Web copiada

---
+++ src/playerRouter.js
@@ -0,0 +1,113 @@
+import express from 'express';
+import * as playerService from './playerService.js';
+import { Player, Subelement } from './defaultPlayers.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('plantilla', {
+        players: playerService.getPlayers(),
+    });
+});
+
+router.get("/formulario", (req, res) => {
+    res.render('formulario');
+});
+
+router.post('/crear', (req, res) => {
+    let player = new Player(
+        req.body.photo,
+        req.body.name,
+        req.body.position,
+        req.body.number,
+        req.body.birthday,
+        req.body.nationality,
+        req.body.price,
+        req.body.description,
+    )
+
+    if (playerService.correctValues(player)) { //si no ha habido ningún error, se crea el nuevo elemento(jugador)
+        playerService.addPlayer(player);
+        res.render('mensajes', {
+            title: "Ficha creada",
+            message: "Jugador añadido correctamente"
+        });
+    }
+});
+
+router.get('/ficha', (req, res) => {
+    let id = parseInt(req.query.id);
+    let player = playerService.getPlayer(id);
+    res.render('ficha', {
+        player: player,
+        name: player.name.normalize('NFD').replace(/[\u0300-\u036f]/g, ''),  // h2 muestra mal las tildes, así conseguimos quitarlas
+        subelems: player.subelements
+    });
+});
+
+router.get('/borrar', (req, res) => {
+    let id = parseInt(req.query.id);
+    let name = playerService.getPlayer(id).name;
+
+    playerService.deletePlayer(id);
+
+    res.render('mensajes', {
+        title: "Ficha eliminada",
+        message: "Ficha de " + name + " eliminada definitivamente"
+    });
+});
+
+router.get('/editar', (req, res) => {
+    let id = parseInt(req.query.id)
+    let player = playerService.getPlayer(id);
+    res.render('formulario');
+});
+
+router.post('/fichaEditada', (req, res) => {
+    let newPlayer = new Player(
+        req.body.playerphoto,
+        req.body.name,
+        req.body.position,
+        req.body.jerseyNumber,
+        req.body.birthday,
+        req.body.nationality,
+        req.body.price,
+        req.body.description,
+    );
+
+    if (playerService.correctValues(newPlayer)) {
+        let player = playerService.getPlayer(parseInt(id));
+
+        playerService.editPlayer(player, newPlayer);
+
+        res.render('mensajes', {
+            title: "Ficha editada",
+            message: "Ficha editada correctamente"
+        });
+    }
+});
+
+router.post("/subelementoCreado", (req, res) => {
+    let sub = new Subelement(
+        req.body.emblem,
+        req.body.club,
+        req.body.start,
+        req.body.end
+    )
+
+    let id = parseInt(req.body.id);
+
+    if (playerService.correctSubvalues(sub)) {
+        let player = playerService.getPlayer(id);
+
+        player.addSubelement(sub);
+
+        res.render('ficha', {
+            player: player,
+            name: player.name.normalize('NFD').replace(/[\u0300-\u036f]/g, ''),
+            subelems: player.subelements
+        });
+    }
+});
+
+export default router;
---
+++ src/playerService.js
@@ -0,0 +1,88 @@
+let players = new Map();
+let nextId = 0;
+let freesIdArray = [];
+let freesIdSet = new Set();
+
+export function addPlayer(player) {
+    player.id = freesIdArray.length ? freesIdArray.pop() : nextId++;  // Si hay ids que han quedado libres se usará uno de ellos, sino se creará uno nuevo
+    players.set(player.id, player);
+}
+
+export function editPlayer(oldPlayer, newPlayer){
+    newPlayer.subelements = oldPlayer.subelements;
+    newPlayer.id = oldPlayer.id;
+
+    players.set(newPlayer.id, newPlayer);
+}
+
+export function deletePlayer(id) {
+    if (id >= nextId || freesIdSet.has(id)) throw new Error("Invalid id");
+
+    players.delete(id);
+
+    freesIdArray.push(id);
+    freesIdSet.add(id);
+}
+
+export function getPlayers() {
+    return [...players.values()]; // == Array.from(players.values())
+}
+
+export function getPlayer(id) {
+    return players.get(id);
+}
+
+function validFormat(url) {
+    const regex = /^(ftp|http|https):\/\/[^ "]+$/;
+    return regex.test(url);
+}
+
+function hasImageExtension(url) {
+    const validExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+    const extension = url.substring(url.lastIndexOf('.')).toLowerCase();
+    return validExtensions.includes(extension);
+}
+
+export function correctValues(player) {
+    let number = parseInt(player.number);
+    let marketValue = parseInt(player.marketValue);
+
+    console.log(player.birth)
+
+    let correct = false;
+
+    if (player.name == "" || player.number == "" || player.nationality == "" || player.description == "") {
+        alert("Rellene todos los campos del formulario");
+    } else if (isNaN(number) || !Number.isInteger(number) || number < 0) {
+        alert("El dorsal debe ser un número mayor o igual a 0");
+    } else if (marketValue == 0) {
+        alert("Todos los jugadores valen algo");
+    } /*else if (!validFormat(player.photo) || !hasImageExtension(player.photo)) {
+        alert("No es una URL de imagen válida");  //para cuando cambiemos las imágenes a urls
+    } */else {
+        correct = true;
+    }
+
+    return correct;
+}
+
+export function correctSubvalues(sub) {
+    let start = parseInt(sub.stage.start);
+    let end = parseInt(sub.stage.end);
+
+    let correct = false;
+
+    if (sub.club == "" || start == "" || end == "") {
+        alert("Complete todos los campos del formulario");
+    } else if (isNaN(start) || !Number.isInteger(start) || isNaN(end) || !Number.isInteger(end)) {
+        alert("La etapa en el club deben ser dos años, es decir, dos números enteros");
+    } else if (start > end) {
+        alert("¡Es imposible que haya dejado el club antes de haber entrado!");
+    } /*else if (!validFormat(sub.emblem) || !hasImageExtension(sub.emblem)) {
+        alert("No es una URL de imagen válida");  //para cuando cambiemos las imágenes a urls
+    } */else {
+        correct = true;
+    }
+
+    return correct;
+}
